e6c400a1fecc5068a2ff85c006fac0a735118846
Ade Priyanto <mr.adepriyanto@gmail.com>
2024-12-24 04:25:15 +0700

    Initial create tanggal;

--- IndonesiaDate.php
+++ IndonesiaDate.php
@@ -12,10 +12,19 @@ class IndonesiaDate extends Model
     public function __construct($config = [])
     {
         date_default_timezone_set("Asia/Jakarta");
+        setlocale(LC_ALL, 'id');
     }
 
     public static function now(){
         $now = date("Y-m-d H:i:s");
         return $now;
+    }
+    public static function create($datetime,$format = 'Y-m-d H:i:s')
+    {
+        $exp = explode(" ", $datetime);
+        $date = $exp[0];
+        $time = $exp[1];
+        $datetime = new \DateTime();
+        return $datetime->format($format);
     }
 }
